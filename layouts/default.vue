396209d5d95da45b389c94ceb22084e781f0dcda
Puritechlab <pub@puritechlab.com>
2025-08-08 10:07:13 +0900

    파비콘 삽입

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,3 +2,13 @@
   <NuxtPage />
   <Footer/>
 </template>
+
+<script setup>
+
+useHead({
+  link: [
+    { rel: 'icon', type: 'image/png', href: '/favicon.png' }
+  ]
+})
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,12 @@ export default defineNuxtConfig({
     '@/assets/css/main.css',
     '@/assets/css/common.css',
   ],
+  app: {
+    head: {
+      title: '뉴스피릿 공모전',
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' }
+      ]
+    }
+  }
 })
